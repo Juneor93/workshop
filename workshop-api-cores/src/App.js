d9b7e15db1a8274bf5322d3350521a527f7fdccb
Raquel Moreira <raquel.costt@gmail.com>
2020-09-01 06:36:54 -0300

    Componentes

--- workshop-api-cores/src/App.js
+++ workshop-api-cores/src/App.js
@@ -1,38 +1,20 @@
 import React from 'react';
 import './App.css';
+import Header from './components/header';
+import Footer from './components/footer';
+import Api from './components/api';
 
 class App extends React.Component {
-  state = {
-      paleta: []
-  };
-
-  componentDidMount() {
-      fetch('http://www.colr.org/json/colors/random/60')
-          .then(res => res.json())
-          .then(res => {
-              const paleta = res.colors.map((item) => item.hex).filter( (item) => item) 
-              this.setState({paleta});
-          });
-  }
-
-  render() {
-      let content = '' ;
-
-      if (this.state.paleta.length !== 0) {
-          content = (<div className="container">
-              {this.state.paleta.map(item => (
-                  <div style={{backgroundColor: "#" + item, width:'150px',height: '150px', margin: '20px' }}># {item}</div>
-              ))}
-              </div>)
-      }
-
+    render() {
       return (
-          <div>
-          {content}
-          </div>
+        <div className="App">
+          <Header/>
+          <Api/>
+          <Footer/>
+        </div>
       );
+    }
   }
-}
 
 export default App;
 
---
+++ workshop-api-cores/src/components/footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Footer = () => (
+
+    <footer className="rodape">
+        <div className="proj">
+        <h2>Projeto Senai API-React</h2>
+        </div>
+        <div className="dev">
+        <h2>Desenvolvedores</h2>
+        <ul>
+        <li><a href="#">Luan Tavares</a></li>
+        <li><a href="#">Raquel Moreira</a></li>
+        <li><a href="#">Artur</a></li>
+        <li><a href="#">Matheus Motta</a></li>
+        <li><a href="#">Leonardo</a></li>
+        <li><a href="#">Rosana Araújo</a></li>
+        </ul>
+        </div>
+    </footer>
+
+);
+
+export default Footer;
